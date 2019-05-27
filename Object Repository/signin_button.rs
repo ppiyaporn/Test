0aa9e0b9febc09371ba3d4571b898909348880ac
MooMai <MooMai@172.20.10.10>
2019-05-27 23:09:19 +0700

    Test Login V.2

---
+++ Object Repository/signin_button.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>signin_button</name>
+   <tag></tag>
+   <elementGuidId>0bd24d82-2304-42a5-b5e6-46c0f69e6f3c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'id=&quot;login-btn&quot;']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>id=&quot;login-btn&quot;</value>
+   </webElementProperties>
+</WebElementEntity>
